6b7f793affac904be13a4595837c680917d3417c
xiaotian <chiguayeshao@gmail.com>
2023-06-27 23:27:06 +0800

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -22,41 +22,47 @@ const List = () => {
   return (
     <div>
       <Card>
-        <CardHeader className="flex flex-row items-center gap-4">
-          <CardTitle>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </CardTitle>
-          <div className="flex flex-row justify-center items-center gap-4">
-            <CardDescription className="font-semibold text-lg">
-              Harry the Helper
-            </CardDescription>
-            <CardDescription className="text-base">2 hours ago</CardDescription>
+        <div className="flex flex-row">
+          <CardHeader>
+            <CardTitle>
+              <Avatar className="rounded-sm h-16 w-16">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </CardTitle>
+          </CardHeader>
+          <div className="p-6 pl-0">
+            <CardContent>
+              <div className="flex flex-row justify-between items-center gap-4">
+                <div className="flex flex-row justify-between items-center gap-4">
+                  <CardDescription className="font-semibold text-lg">
+                    Harry the Helper
+                  </CardDescription>
+                  <CardDescription className="text-base">
+                    2 hours ago
+                  </CardDescription>
+                </div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                      <span className="sr-only">Open menu</span>
+                      <DotsHorizontalIcon className="h-4 w-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              <p>Card Content Card ContentCard ContentCard Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
           </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <DotsHorizontalIcon className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Edit</DropdownMenuItem>
-              <DropdownMenuItem>Delete</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </CardHeader>
-        <CardContent>
-          <p className="indent-12">
-            Card Content Card ContentCard ContentCard Content
-          </p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
+        </div>
       </Card>
     </div>
   )
