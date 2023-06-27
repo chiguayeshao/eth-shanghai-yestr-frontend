8d2fffa7ca8e4cb3559ea73edea540c15ba60f28
xiaotian <chiguayeshao@gmail.com>
2023-06-27 21:17:38 +0800

    update list

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -8,22 +8,51 @@ import {
   CardTitle
 } from "@/components/ui/card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import { DotsHorizontalIcon } from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
 
 const List = () => {
   return (
     <div>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-
       <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+        <CardHeader className="flex flex-row items-center gap-4">
+          <CardTitle>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </CardTitle>
+          <div className="flex flex-row justify-center items-center gap-4">
+            <CardDescription className="font-semibold text-lg">
+              Harry the Helper
+            </CardDescription>
+            <CardDescription className="text-base">2 hours ago</CardDescription>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <DotsHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+              <DropdownMenuItem>Delete</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <p className="indent-12">
+            Card Content Card ContentCard ContentCard Content
+          </p>
         </CardContent>
         <CardFooter>
           <p>Card Footer</p>
