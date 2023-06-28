d39645b961192e8a9bfe12080be532ba131b884c
xiaotian <chiguayeshao@gmail.com>
2023-06-28 15:10:29 +0800

    add page

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 
 const SideBar = () => {
-  const [activeItem, setActiveItem] = useState("Contributions")
   const router = useRouter()
-
-  const handleClick = (item) => {
-    router.push("/" + item)
-    setActiveItem(item)
-  }
+  const [activeItem, setActiveItem] = useState("")
 
   const menuItems = ["Contributions", "Contributors", "Dashboard"]
+
+  const handleClick = (item) => {
+    if (item === "Contributions") {
+      router.push("/")
+    } else {
+      router.push("/" + item.toLowerCase())
+    }
+  }
+
+  useEffect(() => {
+    if (router.pathname === "/") {
+      setActiveItem("Contributions")
+    } else {
+      setActiveItem(
+        router.pathname.slice(1).charAt(0).toUpperCase() +
+          router.pathname.slice(2)
+      )
+    }
+  }, [router.pathname])
+
   return (
     <div>
       <ul>
