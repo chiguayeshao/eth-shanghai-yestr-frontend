dbdd55ab64bb3c622e553f6f6b4259fff9958c79
xiaotian <chiguayeshao@gmail.com>
2023-06-28 12:21:06 +0800

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -15,7 +15,13 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu"
-import { DotsHorizontalIcon } from "@radix-ui/react-icons"
+import {
+  DotsHorizontalIcon,
+  SketchLogoIcon,
+  ArrowUpIcon,
+  ArrowDownIcon,
+  LinkNone2Icon
+} from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 
 const List = () => {
@@ -35,7 +41,7 @@ const List = () => {
             <CardContent>
               <div className="flex flex-row justify-between items-center gap-4">
                 <div className="flex flex-row justify-between items-center gap-4">
-                  <CardDescription className="font-semibold text-lg">
+                  <CardDescription className="font-semibold text-lg text-black">
                     Harry the Helper
                   </CardDescription>
                   <CardDescription className="text-base">
@@ -56,10 +62,37 @@ const List = () => {
                   </DropdownMenuContent>
                 </DropdownMenu>
               </div>
-              <p>Card Content Card ContentCard ContentCard Content</p>
+              <p>
+                Card Content Card ContentCard ContentCard Card Content Card
+                ContentCard ContentCardCard Content Card ContentCard ContentCard{" "}
+              </p>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
+            <CardFooter className="flex flex-row justify-between">
+              <div className="flex flex-row gap-4">
+                <Button
+                  variant="outline"
+                  className="bg-orange-100 text-orange-600 hover:bg-orange-200 hover:text-orange-600"
+                >
+                  <SketchLogoIcon className="mr-2 h-4 w-4" />
+                  250
+                </Button>
+                <Button
+                  variant="outline"
+                  className="bg-blue-100 text-blue-400 hover:bg-blue-200 hover:text-blue-400"
+                >
+                  <LinkNone2Icon className="mr-2 h-4 w-4" />
+                  linklinklinklinktr.ee/lidang
+                </Button>
+              </div>
+              <div className="flex flex-row gap-4 text-black">
+                <Button variant="outline">
+                  <ArrowUpIcon className="mr-2 h-4 w-4" /> Upvote 12
+                </Button>
+                <Button variant="outline">
+                  <ArrowDownIcon className="mr-2 h-4 w-4" />
+                  Downvote 3
+                </Button>
+              </div>
             </CardFooter>
           </div>
         </div>
